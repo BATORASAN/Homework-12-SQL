e4c41736470cf36631f01456aed03cc60d93df84
Tora <waltva0@gmail.com>
2021-10-13 20:41:19 -0500

    Added initial file structure.

---
+++ script.js
@@ -0,0 +1,52 @@
+switch(choice){
+    case "View All Departments":
+       viewAllDepartments()
+        break;
+    
+    case "View All Roles":
+       viewAllRoles()
+        break;
+    
+    case "View All Employees":
+        viewAllEmployees()
+        break;
+    
+    case "Add A Department":
+        addDepartment()
+        break;
+    
+    case "Add A Role":
+        addRole()
+        break;
+    
+    case "Add An Employee":
+        addEmployee()
+        break;
+    
+    case "Update an Employee Role":
+        updateEmployeeRole()
+        break;
+}
+
+async function viewAllDepartments(){
+
+}
+
+async function viewAllRoles(){
+
+}
+async function viewAllEmployees(){
+
+}
+async function addDepartment(){
+
+}
+async function addRole(){
+
+}
+async function addEmployee(){
+
+}
+async function updateEmployeeRole(){
+
+}
